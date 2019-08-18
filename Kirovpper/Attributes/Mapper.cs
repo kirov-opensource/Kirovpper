9c2fb39ccc58240e5954f251ecf1d25e47cd1769
big.guo <big.guo@generalbiologic.com>
2019-08-18 19:24:07 +0800

    add attributes

---
+++ Kirovpper/Attributes/Mapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Kirovpper.Attributes {
+  [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+  public class Mapper : Attribute {
+    //public Mapper([CallerMemberName] string memberName) {
+    //}
+
+    /// <summary>
+    /// 来源类型
+    /// </summary>
+    private Type SourceType { get; set; }
+    /// <summary>
+    /// 来源列
+    /// </summary>
+    private string SourceColumn { get; set; }
+  }
+}
---
+++ Kirovpper/Attributes/MapperIgnore.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kirovpper.Attributes {
+  public class MapperIgnore : Attribute {
+  }
+}
